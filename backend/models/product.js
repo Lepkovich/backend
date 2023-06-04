8d4c4f42432dbaf9512efd1294a0c8aab11edf26
Lepkovich <6332133@gmail.com>
2023-06-04 10:16:37 +0300

    1.06h of lesson. rest api done for backend

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,14 +2,128 @@ const fs = require('fs'); //подключим модуль file system
 const path = require('path'); //подключим модуль path
 
 class ProductModel {
+    static path = path.join( __dirname, '../db', 'products.json'); //вынесли пусть в переменную path
+
     static async findAll() {
         return new Promise((resolve, reject) => {
-            fs.readFile(path.join( __dirname, '../db', 'products.json'), //прочитаем файл по сформированному пути
+            fs.readFile(this.path, //прочитаем файл по сформированному пути
                 'utf-8', (error, data) => { //вызовем колбэк
                 if (error) {
                     reject(error); //обработаем ошибку
                 } else {
                     resolve(JSON.parse(data)); //распарсим данные файла products.json
+                }
+            })
+        });
+    }
+    static async find(id) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, //прочитаем файл по сформированному пути
+                'utf-8', (error, data) => { //вызовем колбэк
+                if (error) {
+                    reject(error); //обработаем ошибку
+                } else {
+                    try {
+                        const products = JSON.parse(data); //распарсим данные файла products.json в products
+                        const product = products.find(item => parseInt(item.id) === parseInt(id));
+                        //по id передадим продукт в product, предварительно приведя их к целым числам
+                        if(product) {
+                            resolve(product);
+                        } else {
+                            reject("Продукт не найден"); //обработаем ошибку
+                        }
+                    } catch (e) {
+                        reject('невозможно обработать данные'); //обработаем ошибку
+                    }
+                }
+            })
+        });
+    }
+    static async create(newProduct) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, //прочитаем файл по сформированному пути
+                'utf-8', (error, data) => { //вызовем колбэк
+                if (error) {
+                    reject(error); //обработаем ошибку
+                } else {
+                    try {
+                        const products = JSON.parse(data); //распарсим данные файла products.json в products
+                        products.push(newProduct);//добавим новый продукт
+                        const newJSON = JSON.stringify(products); //упаковываем products обратно в json
+                        fs.writeFile(this.path, newJSON, 'utf-8', (error, data) => {
+                            if (error) {
+                                reject(error); //обработаем ошибку
+                            } else {
+                                resolve(newProduct);
+                            }
+                        })
+                    } catch (e) {
+                        reject('невозможно обработать данные'); //обработаем ошибку
+                    }
+                }
+            })
+        });
+    }
+    static async change(id, newProduct) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, //прочитаем файл по сформированному пути
+                'utf-8', (error, data) => { //вызовем колбэк
+                if (error) {
+                    reject(error); //обработаем ошибку
+                } else {
+                    try {
+                        const products = JSON.parse(data); //распарсим данные файла products.json в products
+                        const productIndex = products.findIndex(item => parseInt(item.id) === parseInt(id));
+                        //по id передадим индекс в productIndex, предварительно приведя их к целым числам
+                        if (productIndex > -1) { //первый правильный индекс - это 0
+                            products[productIndex] = newProduct;
+                            const newJSON = JSON.stringify(products); //упаковываем products обратно в json
+                            fs.writeFile(this.path, newJSON, 'utf-8', (error, data) => {
+                            //перезапишем наш json файл
+                                if (error) {
+                                    reject(error); //обработаем ошибку
+                                } else {
+                                    resolve(newProduct);
+                                }
+                            })
+                        } else {
+                            reject("Продукт не найден"); //обработаем ошибку
+                        }
+                    } catch (e) {
+                        reject('невозможно обработать данные'); //обработаем ошибку
+                    }
+                }
+            })
+        });
+    }
+    static async delete(id) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.path, //прочитаем файл по сформированному пути
+                'utf-8', (error, data) => { //вызовем колбэк
+                if (error) {
+                    reject(error); //обработаем ошибку
+                } else {
+                    try {
+                        const products = JSON.parse(data); //распарсим данные файла products.json в products
+                        const productIndex = products.findIndex(item => parseInt(item.id) === parseInt(id));
+                        //по id передадим индекс в productIndex, предварительно приведя их к целым числам
+                        if (productIndex > -1) { //первый правильный индекс - это 0
+                            products.splice(productIndex, 1);//метод удаления 1 продукта из массива
+                            const newJSON = JSON.stringify(products); //упаковываем products обратно в json
+                            fs.writeFile(this.path, newJSON, 'utf-8', (error, data) => {
+                            //перезапишем наш json файл
+                                if (error) {
+                                    reject(error); //обработаем ошибку
+                                } else {
+                                    resolve();
+                                }
+                            })
+                        } else {
+                            reject("Продукт не найден"); //обработаем ошибку
+                        }
+                    } catch (e) {
+                        reject('невозможно обработать данные'); //обработаем ошибку
+                    }
                 }
             })
         });
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,6 +3,10 @@ const express = require('express') // импортируем модуль expres
 
 const router = express.Router();
 
-router.get('/', ProductController.getProducts);
+router.get('/', ProductController.getProducts); //запрос и формирование урла для всех продуктов
+router.get('/:id', ProductController.getProduct); //запрос и формирование урла для отдельного продукта
+router.post('/', ProductController.addProduct); //добавление продукта
+router.put('/:id', ProductController.changeProduct); //изменение продукта
+router.delete('/:id', ProductController.deleteProduct); //удаление продукта
 
 module.exports = router;
